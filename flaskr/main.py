24d06a3248be1a763b8e44c6c4f84ff12f83255d
Giuseppe Giovanelli <giuseppe.giovanelli@bancobmg.com.br>
2025-03-30 11:27:39 -0300

    tests: add conf so there is no redirect and tests pass

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,13 +4,13 @@ from flaskr import users_view
 
 app, db = create_app()
 app.extensions["db"] = db
+app.url_map.strict_slashes = False
 app.register_blueprint(users_view.bp_users, url_prefix="/users")
 
 @app.route("/", methods=["GET"])
 def first_page() -> str:
     """First page of the app. It returns a string."""
     return "This is Giuseppe's Flask app"
-
 
 def get_app():
     """
